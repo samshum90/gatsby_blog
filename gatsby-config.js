0e9622792b8fda6799d0749005add7eab42f1c02
Sam <samshum90@gmail.com>
2021-04-02 16:27:28 +0100

    added third post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/blog`,
+  pathPrefix: `/gatsby_blog`,
   siteMetadata: {
     title: `Samuel Shum`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
